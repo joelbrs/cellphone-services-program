b3978ccc2c6a7ad914392cfb56808e2da316e5df
Joel Filho <joelfilho02812@gmail.com>
2023-02-27 18:02:41 -0300

    add database connection & model structure

---
+++ backend/src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const router = express.Router()
+
+// router.get('/services', (req, resp) => resp.status(200).send('router is working')) 
+
+module.exports = router
